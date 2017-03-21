626d47cd6201652436c95f68c272e5ce846af63d
vitaliy <viprogramm@gmail.com>
2017-03-21 11:21:33 +0300

    fix commander

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,8 +3,12 @@
 import program from 'commander';
 
 program
-  .version('0.0.2')
-  .usage('[options] <first_config> <second_config>')
+  .version('0.0.3')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
+  .arguments('<first_config> <second_config>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig);
+    console.log(secondConfig);
+  })
   .parse(process.argv);
